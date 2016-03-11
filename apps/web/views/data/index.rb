02f98809a961ad7fb6dffd544bb92124e4c12bcb
Marek Waligórski <marek.waligorski@netguru.pl>
2016-03-12 00:30:15 +0100

    show full range of dates + respective data

--- apps/web/views/data/index.rb
+++ apps/web/views/data/index.rb
@@ -3,20 +3,44 @@ module Web::Views::Data
     include Web::View
 
     def labels
-      raw points
-        .map(&:day)
-        .map { |d| d.strftime('%e %b') }
-        .to_json
+      raw(dates.map do |d|
+        d.strftime('%e %b')
+      end.to_json)
     end
 
     def series
-      raw points.map(&:value).to_json
+      raw(current_user.metrics.map do |metric|
+        metric_points = []
+
+        dates.each do |date|
+          point = all_points.find { |p| p.metric_id == metric.id && p.day.to_date == date}
+          if point
+            metric_points << point.value
+          else
+            metric_points << nil
+          end
+        end
+
+        metric_points
+      end.to_json)
     end
 
     private
 
-    def points
-      current_user.metrics.first.points
+    def dates
+      (start_date..end_date)
+    end
+
+    def all_points
+      @all_points ||= current_user.metrics.map(&:points).flatten
+    end
+
+    def start_date
+      all_points.map(&:day).min.to_date
+    end
+
+    def end_date
+      all_points.map(&:day).max.to_date
     end
   end
 end
